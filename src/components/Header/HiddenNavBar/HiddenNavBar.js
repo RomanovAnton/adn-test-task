73322efa3220574f8dd72fb173c1d69989cb4e73
Anton Romanov <rad250495@yandex.ru>
2022-11-13 01:36:59 +0700

    feat: add Header adaptive markup

--- src/components/Header/HiddenNavBar/HiddenNavBar.js
+++ src/components/Header/HiddenNavBar/HiddenNavBar.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { ReactComponent as SearchIcon } from "../../../assets/search-icon.svg";
+import { ReactComponent as CartIcon } from "../../../assets/cart-icon.svg";
+import { ReactComponent as BurgerIcon } from "../../../assets/burger-icon.svg";
 import "./HiddenNavBar.scss";
 
 export const HiddenNavBar = () => {
-  return <div>HiddenNavBar</div>;
+  return (
+    <div className="hidden-navbar">
+      <SearchIcon className="hidden-navbar__icon" />
+      <CartIcon className="hidden-navbar__icon" />
+      <BurgerIcon className="hidden-navbar__icon" />
+    </div>
+  );
 };
--- src/components/Header/NavBar/NavBar.js
+++ src/components/Header/NavBar/NavBar.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { ReactComponent as ProfileIcon } from "../../../assets/profile-icon.svg";
+import { ReactComponent as LikeIcon } from "../../../assets/like-icon.svg";
+import { ReactComponent as CartIcon } from "../../../assets/cart-icon.svg";
 import "./NavBar.scss";
 
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <ProfileIcon className="navbar__icon" />
+      <LikeIcon className="navbar__icon" />
+      <CartIcon className="navbar__icon" />
+    </div>
+  );
 };
